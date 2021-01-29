1af4f4ed13daf93f0fea2022feeb6bd6a488407d
Venkat Shrikrishna <krish@krishlogic.net>
2021-01-29 07:36:08 -0800

    adding static

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
 
     #own apps:
     'pages',
@@ -121,4 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (BASE_DIR/'static',)
